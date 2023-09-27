c6297017ea0265d60ae5f168b7b89e1e0c794e9d
Khegay Artem <aluminaze@gmail.com>
2023-09-28 04:35:25 +0600

    add

--- src/components/tab-egis-map/styles.ts
+++ src/components/tab-egis-map/styles.ts
@@ -23,6 +23,7 @@ export const useStyles = makeStyles()((theme) => ({
     flexDirection: "column",
     overflow: "hidden",
     border: "1px solid red",
+    backgroundColor: "gray",
 
     // [theme.breakpoints.down("lg")]: {
     // maxHeight: `calc(100vh - ${HEADER_DESKTOP_HEIGHT}px - ${MOBILE_BAR_HEIGHT}px)`,
